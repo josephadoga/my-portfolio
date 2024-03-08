99ef49269b9659d7ac861992ca28e1c196111d38
Joseph Adoga <adoga.joseph0@gmail.com>
2024-03-08 11:55:57 -0600

    Project Start

---
+++ assets/script/app.js
@@ -0,0 +1,19 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*                                                       */
+/*  Joseph Adoga (2024)                                  */
+/*  github.com/josephadoga                               */
+/*                                                       */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+'use strict';
+
+// This app requires a server to handle import statements
+// and CORS issues
+import * as utils from './utils.js';
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Organizer                                            */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+utils.print('Welcome');
